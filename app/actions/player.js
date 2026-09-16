64170937bca4f2843600dab30a4fcd61b2dc1b40
Bas Themen <bthemen97@gmail.com>
2026-09-16 16:08:17 +0200

    Fixed fetching current convention

--- app/actions/player.js
+++ app/actions/player.js
@@ -54,7 +54,7 @@ export async function createPlayer(conventionId, formData) {
     }
 
     let targetList = [];
-    for (let i =0; i < NR_TARGETS; i++) {
+    for (let i = 0; i < NR_TARGETS; i++) {
         let newTarget = await getNewTarget(conventionId, appUid);
         if (newTarget) {
             targetList.push(newTarget);
